537670ccbfe9a2f40ddbe3762b19bd23aa90f2d2
Wanzzxx <anjayridwan33@gmail.com>
2026-04-13 10:57:06 +0700

    Add comments for Boss POS in islands table

--- SailorAFK.lua
+++ SailorAFK.lua
@@ -45,13 +45,13 @@ end)
 local islands = {
 	{ name = "Lawless", pos = Vector3.new(54.656269, 6.627283, 1814.841064) },
 	{ name = "Ninja", pos = Vector3.new(-1876.813843, 13.558611, -737.722473) },
-	{ name = "Ninja", pos = Vector3.new(-2109.786865, 12.801344, -596.099854) },
+	{ name = "Ninja", pos = Vector3.new(-2109.786865, 12.801344, -596.099854) }, -- Boss POS
 	{ name = "Judgement", pos = Vector3.new(-1273.102905, 8.520060, -1191.468994) },
 	{ name = "Shinjuku", pos = Vector3.new(-21.131468, 9.205597, -1847.090454) },
 	{ name = "Shinjuku", pos = Vector3.new(669.320557, 9.404799, -1693.271973) },
 	{ name = "Slime", pos = Vector3.new(-1124.683838, 13.918226, 373.355255) },
-	{ name = "Hollow", pos = Vector3.new(-568.870422, -1.921274, 1232.567139) },
-	{ name = "Sailor", pos = Vector3.new(249.287292, 7.593238, 926.742493) },
+	{ name = "Hollow", pos = Vector3.new(-568.870422, -1.921274, 1232.567139) }, -- Boss POS
+	{ name = "Sailor", pos = Vector3.new(249.287292, 7.593238, 926.742493) }, -- Boss POS
 }
 
 local function isAllowed()
